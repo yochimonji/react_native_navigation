7bedc27ce24f1bd8b44b37690d7ef6ad6bc88d96
yochimonji <yochimonji@gmail.com>
2021-12-12 19:40:46 +0900

    add ライフサイクルフックの設定

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, Button } from "react-native";
 import { NavigationProp } from "@react-navigation/native";
+import { useFocusEffect } from "@react-navigation/native";
 
 const HomeScreen = ({ navigation }) => {
-  console.log();
+  useFocusEffect(
+    React.useCallback(() => {
+      console.log("Home Mount");
+      return () => {
+        console.log("Home Unmount");
+      };
+    }, [])
+  );
+
   return (
     <View>
       <Text>ホーム画面</Text>
       <Button
         title="ユーザー"
         onPress={() =>
-          navigation.navigate("User", { userId: 1, userName: "モンジ" })
+          navigation.navigate("User", { userId: 1, userName: "yochimonji" })
         }
       />
     </View>
--- screens/UserScreen.tsx
+++ screens/UserScreen.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 
 const UserScreen = ({ route }) => {
-  console.log(route);
+  useFocusEffect(
+    React.useCallback(() => {
+      console.log("User Mount");
+      return () => {
+        console.log("User Unmount");
+      };
+    }, [])
+  );
+
   return (
     <View>
       <Text>ユーザー画面</Text>
